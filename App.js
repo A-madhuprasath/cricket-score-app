dfc153d323cb747d509bfd455d7f463fe1a3fa30
Madhuveeraprasath <madhu@Madhuveeraprasaths-MacBook-Pro.local>
2022-02-04 15:03:23 +0530

    added common style sheet

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+import style from './assets/globalCss'
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>CricScorer</Text>
+            <Text style={style.text}>Cricket Scorer</Text>
             <StatusBar style="auto"/>
         </View>
     );
---
+++ assets/globalCss.js
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native';
+
+module.exports = StyleSheet.create({
+   text: {
+       color: '#4F44E0',
+   }
+});
