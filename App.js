c612669be735965414fa10d04ffd40432708db0b
vigneshvicky067 <vignesh17881@gmail.com>
2022-02-04 15:18:54 +0530

    added  new linet

--- App.js
+++ App.js
@@ -5,12 +5,10 @@ import style from './assets/globalCss'
 export default function App() {
     return (
         <View style={styles.container}>
-<<<<<<< HEAD
             <Text style={style.text}>Cricket Scorer App</Text>
             <Text style={style.text}>Start match</Text>
-=======
             <Text style={style.text}>Cricket Scorer App starting</Text>
->>>>>>> f23070adfe1dfe9e2f3ba2decdf47d46ff46c2c3
+            <Text style={style.text}>Cricket Scorer App started</Text>
             <StatusBar style="auto"/>
         </View>
     );
