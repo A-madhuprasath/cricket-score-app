7f3fa080bbc70176cf2c778a1cdc324b5e2d6681
vigneshvicky067 <vignesh17881@gmail.com>
2022-02-05 12:49:48 +0530

    created three screens and tab navigation

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import style from './assets/globalCss'
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text style={style.text}>Cricket Scorer App</Text>
-            <Text style={style.text}>Start match</Text>
-            <Text style={style.text}>Cricket Scorer App starting</Text>
-            <Text style={style.text}>Cricket Scorer App started</Text>
-            <StatusBar style="auto"/>
-        </View>
+        <AppNavigator/>
+
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
---
+++ src/screens/NewMatch/NewMatch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View,StyleSheet,Text } from 'react-native';
+
+export function NewMatch(){
+return(
+    <View style={styles.container}>
+        <Text>New match</Text>
+    </View>
+)
+}
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
